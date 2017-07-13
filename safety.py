5454d0a027a1e972a26cf6e91cf944fce6afed60
Dan Shafman <dshafman0900@bths.edu>
2017-07-13 10:18:42 -0400

    Added a bunch of comments

--- safety.py
+++ safety.py
@@ -6,9 +6,10 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 
 class SafetyControllerNode:
-    def __init__(self):
-        # subscribe to incomming Ackermann drive commands
+    def __init__(self): 
+	# subscribe to incoming laser scan data
 	rospy.Subscriber("/scan", LaserScan, self.callback)
+	# subscribe to incomming Ackermann drive commands
         rospy.Subscriber("ackermann_cmd_input", AckermannDriveStamped,
                          self.ackermann_cmd_input_callback)
 
@@ -20,12 +21,14 @@ class SafetyControllerNode:
         self.cmd_pub.publish(msg)
 
     def isClear(self, laser):	
+	# If any object is found within a half meter in a 120deg angle, stop the vehicle
 	for i in range(420, 660):
 	    if laser.ranges[i] < 0.5:
 		return False
 	return True
 
     def callback(self, msg):
+	# Completion handler for laser scan data
 	if self.isClear(msg):
 	    print("go")
 	else:
